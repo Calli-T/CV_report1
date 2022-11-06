f1e34cf75f7938820a49c04ad8ed7f8f700b19df
joy14 <joy14479@gmail.com>
2022-11-07 00:22:02 +0900

    andigh

---
+++ color/colorFilter.py
@@ -0,0 +1,114 @@
+import cv2 as cv
+import numpy as np
+
+
+def mean33(before):
+    after = np.copy(before)
+    filter = np.array([[1 / 9, 1 / 9, 1 / 9], [1 / 9, 1 / 9, 1 / 9], [1 / 9, 1 / 9, 1 / 9]])
+
+    x, y, channel = before.shape
+
+    for i in range(1, x - 1):
+        for j in range(1, y - 1):
+            for c in range(3):
+                after[i, j, c] = 0
+
+                for k in [-1, 0, 1]:
+                    for l in [-1, 0, 1]:
+                        after[i, j, c] += filter[k + 1, l + 1] * before[i + k, j + l, c]
+
+    return after
+
+
+def mean55(before):
+    after = np.copy(before)
+    filter = np.array([[0.04, 0.04, 0.04, 0.04, 0.04], [0.04, 0.04, 0.04, 0.04, 0.04], [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04], [0.04, 0.04, 0.04, 0.04, 0.04]])
+
+    x, y, channel = before.shape
+
+    for i in range(2, x - 2):
+        for j in range(2, y - 2):
+            for c in range(3):
+                after[i, j, c] = 0
+
+                for k in [-2, -1, 0, 1, 2]:
+                    for l in [-2, -1, 0, 1, 2]:
+                        after[i, j, c] += filter[k + 2, l + 2] * before[i + k, j + l, c]
+
+    return after
+
+
+def median33(before):
+    after = np.copy(before)
+    filter = np.zeros([3, 3])
+
+    x, y, channel = before.shape
+
+    for i in range(1, x - 1):
+        for j in range(1, y - 1):
+            for c in range(3):
+                slice = np.sort(np.array(before[i - 1:i + 2, j - 1:j + 2, c]).reshape(1, -1))
+                after[i, j, c] = slice[0][4]
+
+    return after
+
+
+def median55(before):
+    after = np.copy(before)
+    filter = np.zeros([5, 5])
+
+    x, y, channel = before.shape
+
+    for i in range(2, x - 2):
+        for j in range(2, y - 2):
+            for c in range(3):
+                slice = np.sort(np.array(before[i - 2:i + 3, j - 2:j + 3, c]).reshape(1, -1))
+                after[i, j, c] = slice[0][12]
+
+    return after
+
+
+def Laplacian(before):
+    after = np.copy(before)
+    filter = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+    #([[-1, 2, -1], [2, -4, 2], [-1, 2, -1]])
+    #([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+    # ([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
+
+    x, y, channel = before.shape
+
+    for i in range(1, x - 1):
+        for j in range(1, y - 1):
+            for c in range(3):
+                after[i, j, c] = 0
+
+                for k in [-1, 0, 1]:
+                    for l in [-1, 0, 1]:
+                        after[i, j, c] += filter[k + 1, l + 1] * before[i + k, j + l, c]
+
+    return after
+
+
+origin = cv.imread("Lenna.png", cv.IMREAD_COLOR)
+gray = cv.imread("Lenna.png", cv.IMREAD_GRAYSCALE)
+# filtered_mean33=mean33(origin)
+# filtered_mean55=mean55(origin)
+# filtered_median33 = median33(origin)
+# filtered_median55 = median55(origin)
+#filtered_Laplacian = cv.Laplacian(origin, -1)
+#filtered_Laplacian = Laplacian(origin)
+#filtered_Laplacian = cv.Laplacian(origin, -1, ksize=3)
+filtered_Laplacian = cv.Laplacian(gray, -1, ksize=3)
+
+cv.imshow('Origin', origin)
+# cv.imshow('Mean_33', filtered_mean33)
+# cv.imshow('Mean_55', filtered_mean55)
+# cv.imshow('Median_33', filtered_median33)
+# cv.imshow('Median_55', filtered_median55)
+cv.imshow('Laplacian', filtered_Laplacian)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# 남은작업: 테두리 처리, 컬러버전 로직, 라플라시안, UI, 영상
